9979cda37af319d56c6390a43b01ec14b40258be
Rendon Elsesser <rendonelsesser@gmail.com>
2019-03-08 13:51:01 -0500

    tests pass for data types and methods

--- methods.py
+++ methods.py
@@ -6,6 +6,8 @@
 #
 # So if the number 5 is passed in, num_list_with_arg should return [1, 2, 3, 4]
 
+def num_list_with_arg(arg):
+    return list(range (1, arg))
 
 # #2: Modify the has_ruby_exp method below so that it returns a SORTED list of
 # all instructors who have Ruby experience (i.e. where "ruby" == True)
@@ -49,7 +51,10 @@ def has_ruby_exp():
             'pasta': False
         }
     }
-
+    for x in experience:
+        if experience[x]['ruby'] == True:
+            ruby_experience.append(x)
+    return sorted(ruby_experience)
 
 
 # #3: Create a method called toggle_str_num that takes an argument. If the
@@ -58,6 +63,14 @@ def has_ruby_exp():
 # argument is neither a string nor an integer, return the string "this is not a
 # str or a int":
 
+def toggle_str_num(arg):
+    neither = 'this is not a str or a int'
+    if type(arg) is str:
+        return int(arg)
+    elif type(arg) is int:
+        return str(arg)
+    else:
+        return neither
 
 
 # Commit when you finish working on these questions!
