fea947bd2be0456b8477c67301b2a10310175aae
Nathan Rotach <Nathan.Rotach@gmail.com>
2019-06-03 10:06:18 -0400

    here we go

--- methods.py
+++ methods.py
@@ -7,10 +7,15 @@
 # So if the number 5 is passed in, num_list_with_arg should return [1, 2, 3, 4]
 
 
+def num_list_with_arg(upper_boundary):
+    this_range = list(range(1, upper_boundary))
+    return this_range
+
 # #2: Modify the has_ruby_exp method below so that it returns a SORTED list of
 # all instructors who have Ruby experience (i.e. where "ruby" == True)
 # The list should contain only names of instructors.
 # make sure you name the list ruby_experience before returning it.
+
 
 def has_ruby_exp():
     ruby_experience = []
@@ -49,8 +54,10 @@ def has_ruby_exp():
             'pasta': False
         }
     }
-
-
+    # for teacher in experience:
+    #     if experience[teacher] ruby == True
+    #         ruby_experience.append(experience[teacher])
+    # return ruby_experience
 
 # #3: Create a method called toggle_str_num that takes an argument. If the
 # argument is a string, convert it to an integer and return the integer; If the
@@ -58,6 +65,14 @@ def has_ruby_exp():
 # argument is neither a string nor an integer, return the string "this is not a
 # str or a int":
 
+
+def toggle_str_num(my_arg):
+    if type(my_arg) is str:
+        return int(my_arg)
+    if type(my_arg) is int:
+        return str(my_arg)
+    else:
+        return "this is not a str or a int"
 
 
 # Commit when you finish working on these questions!
--- oop.py
+++ oop.py
@@ -13,7 +13,7 @@
 #
 # Your Vehicle class should take one extra argument in the __init__ method (a `dict`) with the above
 # attributes. Define the properties on the class from the dict that is passed in.
-# 
+#
 # Here's an example of the dict that will be passed in to your class:
 #
 # vehicle_dict_vehicle = {
@@ -28,6 +28,30 @@
 # }
 
 
+class Vehicle:
+    def __init__(self, initial_data):
+        # - `vehicle_type`
+        self.vehicle_type = initial_data['vehicle_type']
+        # - `wheel_count`
+        self.wheel_count = initial_data['wheel_count']
+        # - `name`
+        self.name = initial_data['name']
+        # - `mpg`, a 'dict', with the following properties:
+        self.mpg = initial_data['mpg']
+
+        # - `get_vehicle_type` should return the `vehicle_type`
+    def get_vehicle_type(self):
+        return self.vehicle_type
+
+        # - `get_vehicle_drive` if the `wheel_count` for that class is "no wheels!" then
+    def get_vehicle_drive(self):
+        wheels = self.wheel_count
+        if wheels == "no wheels!":
+            #     it should return "no wheels send it back to the shop"
+            return "no wheels send it back to the shop"
+        else:
+            #     otherwise it should return "I have {self.wheel_count} wheel drive" as a formatted string
+            return ("I have %s wheel drive" % (wheels,))
 
 # #2: Create a Motorcycle class that inherits from the Vehicle class and has the
 # following properties and methods:
@@ -36,16 +60,40 @@
 #       otherwise return "popped a wheelie!"
 
 
+def Motorcycle(Vehicle):
+    def __init__(self, initial_data):
+        super().__init__(initial_data)
 
-# #3: Define a Car class that inherits from the Vehicle class with the following properties and methods:
-# - all the properties inherited from the Vehicle class
-# - property: `wheel_count` defaults to 4
-# - method: `can_drive` that should return 'Vrrooooom Vroooom'
+    def pop_wheelie(self):
+        if self.wheel_count != 2:
+            return False
+        else:
+            return "poppead a wheelie!"
+
+    # #3: Define a Car class that inherits from the Vehicle class with the following properties and methods:
+    # - all the properties inherited from the Vehicle class
+    # - property: `wheel_count` defaults to 4
+    # - method: `can_drive` that should return 'Vrrooooom Vroooom'
 
 
-# #4: Define a Truck class that inherits from the Vehicle class with the following properties and methods:
-# - all the properties inherited from the Vehicle class
-# - method: `rev_engine` that should return a string 'rreevv!'
+def Car(Vehicle):
+    def __init__(self, initial_data):
+        super().__init__(initial_data)
+        self.wheel_count = 4
+
+    def can_drive(self):
+        return 'Vrrooooom Vroooom'
+
+        # #4: Define a Truck class that inherits from the Vehicle class with the following properties and methods:
+        # - all the properties inherited from the Vehicle class
+        # - method: `rev_engine` that should return a string 'rreevv!'
 
 
-# Commit when you finish working on these questions!
+def Truck(Vehicle):
+    def __init__(self, initial_data):
+        super().__init__(initial_data)
+
+    def rev_engine(self):
+        return "rreevv!"
+
+        # Commit when you finish working on these questions!
