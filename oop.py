bbbf5b96972221daeadaa7e0b0a532046bb61968
Rasha Baig <rasha.baig@gmail.com>
2019-06-03 10:02:41 -0400

    submit

--- oop.py
+++ oop.py
@@ -13,7 +13,7 @@
 #
 # Your Vehicle class should take one extra argument in the __init__ method (a `dict`) with the above
 # attributes. Define the properties on the class from the dict that is passed in.
-# 
+#
 # Here's an example of the dict that will be passed in to your class:
 #
 # vehicle_dict_vehicle = {
@@ -28,6 +28,21 @@
 # }
 
 
+class Vehicle:
+    def __init__(self, dictionary):
+        self.dict = dictionary
+
+    def get_vehicle_type():
+        return(vehicle_type)
+
+    def get_vehicle_drive():
+        if wheel_count == "no wheels!":
+            return('no wheels send it back to the shop')
+        else:
+            return("I have {self.wheel_count} wheel drive")
+
+
+me = Vehicle(vehicle_dict_vehicle)
 
 # #2: Create a Motorcycle class that inherits from the Vehicle class and has the
 # following properties and methods:
@@ -36,6 +51,15 @@
 #       otherwise return "popped a wheelie!"
 
 
+class Motorcycle(Vehicle):
+    def __init__(self):
+    super().__init__()
+
+    def pop_wheelie():
+        if wheel_count != 2:
+            return False
+        else:
+            return("popped a wheelie!")
 
 # #3: Define a Car class that inherits from the Vehicle class with the following properties and methods:
 # - all the properties inherited from the Vehicle class
@@ -43,9 +67,22 @@
 # - method: `can_drive` that should return 'Vrrooooom Vroooom'
 
 
+class Car(Vehicle):
+    def __init__(self):
+    super().__init__()
+
+    def can_drive():
+        return('Vrrooooom Vroooom')
 # #4: Define a Truck class that inherits from the Vehicle class with the following properties and methods:
 # - all the properties inherited from the Vehicle class
 # - method: `rev_engine` that should return a string 'rreevv!'
 
+
+class Truck(Vehicle):
+    def __init__(self):
+    super().__init__()
+
+    def rev_engine():
+        return('rreevv!')
 
 # Commit when you finish working on these questions!
