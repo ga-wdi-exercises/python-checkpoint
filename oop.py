7d75929f7b5643cf44ce22df6e02288660bef485
Daniel Woldehana <daniel.woldehana@yahoo.comM>
2019-06-03 10:40:54 -0400

    update

--- oop.py
+++ oop.py
@@ -13,7 +13,7 @@
 #
 # Your Vehicle class should take one extra argument in the __init__ method (a `dict`) with the above
 # attributes. Define the properties on the class from the dict that is passed in.
-# 
+#
 # Here's an example of the dict that will be passed in to your class:
 #
 # vehicle_dict_vehicle = {
@@ -28,13 +28,30 @@
 # }
 
 
+class Vehicle:
+    def __int__(self, vehicle_type, wheel_count, name):
+        self.vehicle_type: vehicle_type
+        self.wheel_count: wheel_count
+        self.name: name
+        self.mpg: {
+            'city': int,
+            'highway': int,
+            'combined': int
+        }
 
+    def get_vehicle_type(self):
+        return self.vehicle_type
+
+    def get_vehicle_drive(self):
+        if self.wheel_count == 'no wheels!':
+            return "no wheels send it back to the shop"
+        else:
+            return "I have {self.wheel_count} wheel drive"
 # #2: Create a Motorcycle class that inherits from the Vehicle class and has the
 # following properties and methods:
 # - all the properties inherited from the Vehicle class
 # - method: `pop_wheelie` if `wheel_count` is not equal to 2 then it should return False
 #       otherwise return "popped a wheelie!"
-
 
 
 # #3: Define a Car class that inherits from the Vehicle class with the following properties and methods:
