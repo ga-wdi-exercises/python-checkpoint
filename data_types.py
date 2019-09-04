ced4d2be3085c0d1b721844b7334d1a205cb578b
Victor Johnson <vjohnson22@gmail.com>
2019-09-04 09:14:00 -0400

    data types

--- data_types.py
+++ data_types.py
@@ -15,11 +15,12 @@ dictionary_classroom = {
 
 
 # #2: Create a list called "num_list" withs values 1 through 4 in it:
-
+num_list = [1,2,3,4]
 
 # #3: Create a list called "teacher_list" with the strings "zakk", "jimmy",
 # "hammad", and "erin" in it:
 
+teacher_list = [ 'zakk', 'jimmy', 'hammad', 'erin']
 
 
 # Commit when you finish working on these questions!
