c5578e2fa92eeddb839cb30513b99294b43d5abf
Domo <delaney_dominique@icloud.com>
2019-10-16 10:20:59 -0400

    update

--- data_types.py
+++ data_types.py
@@ -4,14 +4,30 @@
 #     - "name" -> data_type: string, value: CR3
 #     - "windows_count" -> data_type: int, value: 8
 #     - "glare" -> data_type: boolean, value: True
-
+# class dictionary_classroom:
+#     def __init__(self,  )
+class dictionary_classroom:
+    "chairs" = "35",
+    "name" = "CR3",
+    "windows_count" = "8",
+    "glare" = "true"
+    def description(self):
+        return desc_str
 
 
 # #2: Create a list called "num_list" withs values 1 through 4 in it:
-
+num_list = [1, 2, 3, 4]
 
 # #3: Create a list called "teacher_list" with the strings "zakk", "jimmy",
 # "hammad", and "erin" in it:
+# teacher_list = [zakk, jimmy, hammad, erin]
+# print (teachers_list [0])
+# print (teachers_list [1])
+# print (teachers_list [2])
+# print (teachers_list [3])
+
+teacher_list(zakk, jimmy, hammad, erin)
+['zakk', 'jimmy', 'hammad', 'erin']
 
 
 
